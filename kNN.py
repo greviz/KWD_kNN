61b2350e043adbb7a487e6b77bf7d1b4820f41b9
Damian Król <damian1krol@gmail.com>
2018-10-24 17:26:05 +0200

    Reading data

--- kNN.py
+++ kNN.py
@@ -1,2 +1,21 @@
-print("Git")
-print("Git")
+import numpy as np
+import pandas as pd
+from scipy.spatial.distance import pdist, squareform, cdist
+
+class kNN:
+    def __init__(self,k,nameOfLearningDataFile):
+        self.k=k
+        self.fullLearningData=np.array(pd.read_csv(nameOfLearningDataFile,header=None))
+        self.learningData=self.fullLearningData[:,0:4]
+        self.labels=self.fullLearningData[:,4]
+    def getLearningData(self):
+        return self.learningData
+    def getLabels(self):
+        return self.labels
+
+
+k = kNN(5,"iris.data.learning")
+x = k.getLabels()
+print(x)
+y=k.getLearningData()
+print(y)
